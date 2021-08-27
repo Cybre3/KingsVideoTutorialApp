8c9e7702de56c07378afb5a152835c469420ce36
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2021-08-26 23:17:10 -0400

    basic models complete

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [ true, 'Username is Required, please enter username' ],
+    },
+    password: {
+        type: String,
+        required: [ true, 'Password is Required, please enter password' ],
+    },
+    enrolledCourses: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+userSchema.methods.findUser = function () {
+    return mongoose.model("user").find({ _id: this._id }, function () {
+        console.log("User Found!");
+    });
+};
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
